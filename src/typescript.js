c7da5079e4815faeb64baa56af8559d07b23b840
alwynou <alwynou@163.com>
2023-07-12 17:37:50 +0800

    fix: typescript preset

--- src/typescript.js
+++ src/typescript.js
@@ -21,7 +21,7 @@ export const typescript = [
     },
     rules: {
       ...tsPlugin.configs['eslint-recommended'].overrides[0].rules,
-      ...tsPlugin.configs['recommended'].rules,
+      ...tsPlugin.configs.strict.rules,
       'no-duplicate-imports': 'off', // 与类型单独引入冲突，所以关闭该规则
 
       '@typescript-eslint/no-unused-vars': [
